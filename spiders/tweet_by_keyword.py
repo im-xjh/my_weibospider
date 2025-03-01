bee56f09c96b09dd587d248d07d74f9ce638a1fa
“im-xjh” <im.xjh@outlook.com>
2025-03-02 01:27:10 +0800

    更新说明

--- spiders/tweet_by_keyword.py
+++ spiders/tweet_by_keyword.py
@@ -17,10 +17,10 @@ class TweetSpiderByKeyword(Spider):
         爬虫入口
         """
         # 这里keywords可替换成实际待采集的数据
-        keywords = [""]
+        keywords = ["deepseek"]
         # 这里的时间可替换成实际需要的时间段
-        start_time = datetime.datetime(year=2024, month=7, day=31, hour=0)
-        end_time = datetime.datetime(year=2024, month=7, day=31, hour=0)
+        start_time = datetime.datetime(year=2024, month=12, day=31, hour=0)
+        end_time = datetime.datetime(year=2025, month=7, day=31, hour=0)
         # 是否按照小时进行切分，数据量更大; 对于非热门关键词**不需要**按照小时切分
         is_split_by_hour = True
         for keyword in keywords:
