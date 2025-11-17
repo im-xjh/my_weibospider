b0f7b8e8b275574b1c2a83463b23a042681c9b4b
“im-xjh” <im.xjh@outlook.com>
2025-11-17 21:30:22 +0800

    fix the problem of 20 comment

--- weibospider/spiders/comment.py
+++ weibospider/spiders/comment.py
@@ -1,39 +1,59 @@
 import json
+from urllib.parse import urlencode
+
 from scrapy import Spider
 from scrapy.http import Request
+
 from spiders.common import parse_user_info, parse_time, url_to_mid
+
 
 class CommentSpider(Spider):
     """
     微博评论数据采集
     """
+
     name = "comment"
 
-    def __init__(self, ids_to_process=None, is_single=False, single_id=None, *args, **kwargs):
+    def __init__(self, ids_to_process=None, is_single=False, single_id=None, flow=0, *args, **kwargs):
         """
         :param ids_to_process: 外部文件传入的 mblogid 列表
         :param is_single: 是否只有单个ID
         :param single_id: 单个ID的值（若 is_single=True，则可用 single_id）
+        :param flow: 评论排序模式（0=热度，1=按时间；首条请求不携带 flow）
         """
         super().__init__(*args, **kwargs)
         self.ids_to_process = ids_to_process or []
         self.is_single = is_single
         self.single_id = single_id
+        self.flow = 1 if str(flow) == "1" else 0
 
     def start_requests(self):
         # 如果外部没传IDs，就走内部预设
         if not self.ids_to_process:
             # 这里给一个演示用途的 mblogid
-            self.ids_to_process = ["LbtQJvlnQ"]  # 举例,"","",""
+            self.ids_to_process = ["PoHWDfhAC"]  # 举例
 
         for mblogid in self.ids_to_process:
             mid = url_to_mid(mblogid)
-            url = f"https://weibo.com/ajax/statuses/buildComments?is_reload=1&id={mid}&is_show_bulletin=2&is_mix=0&count=20"
-            yield Request(url, callback=self.parse, meta={'source_url': url, 'mblogin': mblogid})
+            referer = self._build_referer(mblogid)
+            meta = {
+                'mblogin': mblogid,
+                'target_id': mid,
+                'fetch_level': 0,
+                'referer': referer,
+            }
+            yield self._build_comment_request(
+                target_id=mid,
+                fetch_level=0,
+                include_flow=False,
+                meta=meta,
+            )
 
     def parse(self, response, **kwargs):
         mblogin = response.meta.get('mblogin')
+        fetch_level = response.meta.get('fetch_level', 0)
         data = json.loads(response.text)
+
         for comment_info in data.get('data', []):
             item = self.parse_comment(comment_info)
             # 为了在 pipeline 中能将其置于最前面，我们这里就直接命名为 mblogin
@@ -42,16 +62,37 @@ class CommentSpider(Spider):
 
             # 解析二级评论
             if 'more_info' in comment_info:
-                url = (
-                    f"https://weibo.com/ajax/statuses/buildComments?is_reload=1&id={comment_info['id']}"
-                    f"&is_show_bulletin=2&is_mix=1&fetch_level=1&max_id=0&count=100"
+                child_meta = {
+                    'mblogin': mblogin,
+                    'target_id': comment_info['id'],
+                    'fetch_level': 1,
+                    'referer': response.meta.get('referer'),
+                }
+                yield self._build_comment_request(
+                    target_id=comment_info['id'],
+                    fetch_level=1,
+                    include_flow=False,
+                    meta=child_meta,
+                    priority=20,
                 )
-                yield Request(url, callback=self.parse, priority=20, meta={'mblogin': mblogin})
 
         # 翻页
-        if data.get('max_id', 0) != 0 and 'fetch_level=1' not in response.url:
-            url = response.meta['source_url'] + '&max_id=' + str(data['max_id'])
-            yield Request(url, callback=self.parse, meta={'source_url': response.meta['source_url'], 'mblogin': mblogin})
+        max_id = data.get('max_id', 0)
+        if max_id:
+            next_meta = {
+                'mblogin': mblogin,
+                'target_id': response.meta.get('target_id'),
+                'fetch_level': fetch_level,
+                'referer': response.meta.get('referer'),
+            }
+            yield self._build_comment_request(
+                target_id=response.meta.get('target_id'),
+                fetch_level=fetch_level,
+                include_flow=True,
+                meta=next_meta,
+                max_id=max_id,
+                max_id_type=data.get('max_id_type', 0),
+            )
 
     @staticmethod
     def parse_comment(data):
@@ -69,3 +110,49 @@ class CommentSpider(Spider):
                 'user': parse_user_info(data['reply_comment']['user']),
             }
         return item
+
+    def _build_comment_request(self, target_id, fetch_level, include_flow, meta, max_id=0, max_id_type=0, priority=0):
+        params = {
+            'is_reload': 1,
+            'id': target_id,
+            'is_show_bulletin': 2,
+            'is_mix': 1 if fetch_level else 0,
+            'count': 20,
+            'fetch_level': fetch_level,
+        }
+        if include_flow:
+            params['flow'] = self.flow
+        if max_id:
+            params['max_id'] = max_id
+            params['max_id_type'] = max_id_type
+        url = f"https://weibo.com/ajax/statuses/buildComments?{urlencode(params)}"
+
+        headers = self._build_headers(meta.get('referer'))
+        request_meta = {
+            'mblogin': meta.get('mblogin'),
+            'target_id': target_id,
+            'fetch_level': fetch_level,
+            'referer': meta.get('referer'),
+        }
+        return Request(
+            url,
+            callback=self.parse,
+            headers=headers,
+            meta=request_meta,
+            dont_filter=True,
+            priority=priority,
+        )
+
+    @staticmethod
+    def _build_headers(referer):
+        headers = {
+            'Referer': referer or 'https://weibo.com/',
+            'X-Requested-With': 'XMLHttpRequest',
+        }
+        return headers
+
+    @staticmethod
+    def _build_referer(mblogid):
+        if not mblogid:
+            return 'https://weibo.com/'
+        return f"https://weibo.com/{mblogid}"
